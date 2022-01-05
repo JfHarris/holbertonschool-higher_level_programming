cc183f993b08681a110f8c7cde08ea0d1179162e
JfHarris <3374@holbertonschool.com>
2022-01-04 16:45:14 -0800

    Added empty space to print statements

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,12 +3,12 @@ def fizzbuzz():
     for x in range(1, 100):
         if x % 5 == 0 and x % 3 == 0:
             x = "Fizzbuzz"
-            print("{:s}".format(x), end="")
+            print("{:s} ".format(x), end="")
         elif x % 5 == 0:
             x = "Buzz"
-            print("{:s}".format(x), end="")
+            print("{:s} ".format(x), end="")
         elif x % 3 == 0:
             x = "Fizz"
-            print("{:s}".format(x), end="")
+            print("{:s} ".format(x), end="")
         else:
-            print("{:d}".format(x), end="")
+            print("{:d} ".format(x), end="")
