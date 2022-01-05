bd3cf4674f67bcde7e5c28d17bc5f6c790a5ed9d
JfHarris <3374@holbertonschool.com>
2022-01-04 16:52:10 -0800

    Made B in FizzBuzz uppercase

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for x in range(1, 101):
         if x % 5 == 0 and x % 3 == 0:
-            x = "Fizzbuzz"
+            x = "FizzBuzz"
             print("{:s} ".format(x), end="")
         elif x % 5 == 0:
             x = "Buzz"
