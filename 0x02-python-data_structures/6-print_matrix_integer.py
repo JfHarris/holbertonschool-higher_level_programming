38666976ad183664dcc85492fd26042e04cdf981
JfHarris <3374@holbertonschool.com>
2022-01-06 18:29:07 -0800

    Add/edit

---
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for total in row:
+                if total == row[0]:
+                    print("{:d}".format(total), end="")
+                else:
+                    print("{:d}".format(total), end="")
+                print()
+            else:
+                print()
