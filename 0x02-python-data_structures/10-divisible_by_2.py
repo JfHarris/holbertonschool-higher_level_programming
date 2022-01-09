f9bd0d8fd15c38f39c635c66b73629ae40648bf6
JfHarris <3374@holbertonschool.com>
2022-01-08 19:06:36 -0800

    Changed + to += in new_list = my_list

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new = my_list[]
-    new = my_list
+    new += my_list
     if my_list:
         for x in range(len(my_list)):
             if (my_list[x] % 2) == 0:
