82944b75a51fe38e1bab0b7223844981ce3417bd
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-11 01:09:19 -0600

    Add/ ~400 edits

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Making a class name Square
+"""
+
+class Square:
+    """
+    class by name of Square
+    size = private var
+    """
+    def __init__(self, size):
+        self.__size = size
