57acdec1cfaddeee93406cee5aaf3deb0ea4706f
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-15 00:09:51 -0600

    Redid. Simplified.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,11 +8,8 @@ def add_integer(a, b=98):
     Add numbers. Raise errors if necessary
     """
 
-    if ((isinstance(a, (int, float))) and isinstance (b, (int, float))):
-        a = int(a)
-        b = int(b)
-        return a + b
-    if (isinstance(a (int, float)) is False):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if (isinstance(b, (int, float)) is False):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    return (int(a) + int(b))
