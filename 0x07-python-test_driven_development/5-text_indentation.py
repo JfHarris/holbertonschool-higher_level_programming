eda771d920ce4ff0592ef26d88a405ac80278de5
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-15 00:22:22 -0600

    Add/edit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Print 2 newlines after instances of  . ? or :
+"""
+
+def text_indentation(text):
+    """
+    Print 2 newlines after instances of . ? or :
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    punc = ""
+    for char in text:
+        if char is " " and char is text[0] and punc is "":
+            punc = "\n"
+            continue
+        if char is " " and punc is "\n":
+            continue
+        if char is "." or char is "?" or char is ":":
+            print(char)
+            punc = "\n"
+        else:
+            print(char, end="")
+            punc = char
