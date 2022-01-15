bdf67fbab69b8941ddfe98ec053d4c7b373bd459
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-14 19:02:39 -0600

    Several edits

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Print a square made of #
+"""
+
+def print_square(size):
+    """
+    Raise errors if needed
+    """
+    if isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if not isinstance(size, (int, float)) and not isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for x in range(0, int(size)):
+        print("#" * (size))
