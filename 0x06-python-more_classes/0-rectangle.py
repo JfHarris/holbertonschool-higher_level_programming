3da8bd9a4086a62286157a4a8c34f8e302019e92
jfharris <jfharris1983@gmail.com>
2022-01-16 23:51:04 -0600

    Overall edit

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -24,25 +24,26 @@ class Rectangle:
         self.__height = height
 
     @property
-    def height(self):
-        return self.__height
-
-    @property
     def width(self):
         return self.__width
 
-    @width.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    @property
+    def height(self):
+        return self.__height
 
-    @height.setter
+    @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+# try switching setter height width
