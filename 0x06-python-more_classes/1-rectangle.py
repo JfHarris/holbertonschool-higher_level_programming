b4b41b58025b22de44f44e6374cbe8274d828df2
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-17 23:33:05 -0600

    Add/several hundred edits

---
+++ 0x06-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Just checking
+"""
+
+
+class Rectangle:
+    """
+    gambling against pep8 and the checker
+    """
+    def __init__(self, width=0, height=0):
+        if width < 0:
+            raise ValueError("width must be >=0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(self.__height, int) or isinstance(self.__height, bool):
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * self.__height) + (2 * self.__width)
