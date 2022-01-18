dd77b6c50906f5daabf10f5cbdfcb3cdb526dd20
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-17 22:40:06 -0600

    Edited for QA review

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -25,6 +25,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([2, 4]), 4)
         self.assertEqual(max_integer([-7, -1, 12]), 12)
         self.assertEqual(max_integer([9, 8, 7]), 9)
+        self.assertEqual(max_integer([-4, -5, -6]), -4)
 
     def test_empty(self):
         """
