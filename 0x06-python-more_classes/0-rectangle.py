3ad18acd76e3a2435467925f8a8f3bf04f0444bb
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-17 23:00:36 -0600

    Switched setter order

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     class Rectangle
     """
 
-    def __init__(self, width=0, height=0):
+    def __init__(self, height=0, width=0):
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
@@ -20,24 +20,16 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
+        self.__width = width
 
     @property
     def height(self):
         return self.__height
 
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+    @property
+    def width(self):
+        return self.__width
 
     @height.setter
     def height(self, value):
@@ -46,4 +38,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-# try switching setter height width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    # try switching setter height width
