3b483cc7756850c66cd76131b6a271076d2afe41
jfharris <jfharris1983@gmail.com>
2022-01-20 20:41:27 -0600

    Added quotes around height and width

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+8-rectangle contains class BaseGeometry, public instance methods
+area and integer_validator
+SubClass Rectangle
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """inherits methods from BaseGeometry
+    """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
