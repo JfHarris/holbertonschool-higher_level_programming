7d2b0f27842ef0332b64af1cead743a8e353ed7d
jfharris <jfharris1983@gmail.com>
2022-01-21 17:56:55 -0600

    Edited docstring

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-append file and print it
+append file and print number of chars
 """
 
 
 def append_write(filename="", text=""):
     """
-    append file and print it
+    append file and print number of chars
     """
     with open(filename, mode="a", encoding="utf-8") as f:
         return(f.write(text))
