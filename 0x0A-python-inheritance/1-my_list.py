d851854cc8d1f973ced16e40708f90c16ff091b6
jfharris <jfharris1983@gmail.com>
2022-01-20 19:54:26 -0600

    Added second docstring

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+inherits from list
+"""
+
+
+class MyList(list):
+    """
+    inherits from list, prints sorted list
+    """
+    def print_sorted(self):
+        print(sorted(self))
