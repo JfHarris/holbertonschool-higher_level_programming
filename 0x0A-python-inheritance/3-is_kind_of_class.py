28729aeca531538678d824814e08cd80a2ce34f1
jfharris <jfharris1983@gmail.com>
2022-01-20 18:02:40 -0600

    Edit 1

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+3-is_kind_of_class is True if obj is same class it inherits from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    3-is_kind_of_class is True if obj is same class it inherits from
+    """
+    return isinstance(obj, a_class)
