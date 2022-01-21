d7ff16275cc0df35145c8625a52a07684dfb4015
jfharris <jfharris1983@gmail.com>
2022-01-20 18:06:59 -0600

    split line 9 and removed whitespace on line 12 for pep8

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,6 +6,7 @@
 
 def is_same_class(obj, a_class):
     """
-    return True if the object is exactly an instance of the specified class ; otherwise False
+    return True if the object is exactly an instance
+    of the specified class ; otherwise False
     """
-    return type (obj) == a_class
+    return type(obj) == a_class
