55e9f9f7af2dcee616e4a7661aa8ba618243245a
jfharris <jfharris1983@gmail.com>
2022-01-20 23:10:31 -0600

    700 edits or so

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+10-square contains class BaseGeometry with public instance methods
+area and integer_validator
+Subclass Rectangle with attributes
+width and height
+Subclass Square with private attribute
+size
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    inherits from Rectangle which inherits from BaseGeometry
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
