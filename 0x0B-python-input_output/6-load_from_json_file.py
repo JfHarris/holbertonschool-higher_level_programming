aae0510e7cc49b52a9bd0f611bf6341ef4bc5b27
jfharris <jfharris1983@gmail.com>
2022-01-21 19:02:24 -0600

    Added 2nd docstring

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+creates an Object from a “JSON file”
+"""
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”
+    """
+    import json
+
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
