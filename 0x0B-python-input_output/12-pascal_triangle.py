ae69a74539b29c126703feaa4090c0aa5e393d68
jfharris <jfharris1983@gmail.com>
2022-01-21 23:09:22 -0600

    Edit 1

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+returns a list of lists of integers representing the Pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascals triangle
+    """
+    if n <= 0:
+        return [0]
+    if n == 1:
+        return [[1]]
+
+    tri = [[1]]
+    for rows in range(n-1):
+        tri.append([x + y for x, y in zip([0] + tri[-1], tri[-1] +[0])])
+    return tri
