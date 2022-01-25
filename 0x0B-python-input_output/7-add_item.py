20d4c9e807817918d5edd7eea3098c7855b3ca17
jfharris <jfharris1983@gmail.com>
2022-01-24 23:09:43 -0600

    rework

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,7 +13,7 @@ with open("add_item.json", 'a+') as f:
     f.seek(0)
     read_data = f.read()
     if read_data == "":
-        json.dump([], d=f)
+        json.dump([], f)
 
 new = load_from_json_file("add_item.json")
 for x in range(1,size):
