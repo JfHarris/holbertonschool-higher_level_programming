990485cb74870b240790206023a1e1a7b8c554ee
jfharris <jfharris1983@gmail.com>
2022-01-24 22:54:42 -0600

    Added import os.path

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ adds all arguments to a Python list, and then save them to a file
 
 import json
 from sys import argv
+import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
