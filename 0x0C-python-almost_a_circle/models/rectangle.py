651cfccede563afd8eaa65ff862822ccdacd4ad0
jfharris <jfharris1983@gmail.com>
2022-01-25 23:53:45 -0600

    1-3 edits

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+class Rectangle
+"""
+
+
+from models.base import Base
+class Rectangle(Base):
+    """
+    class = Rectangle
+    private attrs = width, height, x, and y
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
