064169ecdc8546411a17ec12b8c6f768752a4ae9
jfharris <jfharris1983@gmail.com>
2022-01-26 00:07:31 -0600

    1st edit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+class Square
+"""
+
+
+from models.rectangle import Rectangle
+class Square:
+    """
+    class = Square
+    inherits private attrs from Rectangle(height, width, x, y
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
