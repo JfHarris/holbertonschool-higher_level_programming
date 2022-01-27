2fe81319ca148071757aef34d3e622eddc33602d
jfharris <jfharris1983@gmail.com>
2022-01-27 16:33:37 -0600

    Added docstrings, imports, and directive comments

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+UnitTest for Square class
+"""
+
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class TestSquare(unittest.TestCase):
+    """
+    unittest for class Square
+    """
+    # correct
+    # incorrect type of args
+    # incorrect number of args
