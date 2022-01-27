5a84b59cb883423a1af25fc9ad2e7a6d13916a3a
jfharris <jfharris1983@gmail.com>
2022-01-27 16:24:51 -0600

    Added docstrings, imports and directive comments

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+UnitTest for Rectangle class
+"""
+
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+class TestRectangle(unittest.TestCase):
+    """
+    unittest for Rectangle class
+    """
+    # correct
+    # all args incorrect
+    # no args
