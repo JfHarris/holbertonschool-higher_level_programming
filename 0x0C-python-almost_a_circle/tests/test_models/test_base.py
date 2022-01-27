0cf78e40246b644522ae1d36d300c78c9287a2da
jfharris <jfharris1983@gmail.com>
2022-01-27 16:29:36 -0600

    Added docstrings, imports, and directive comments

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+UnitTest for class Base
+"""
+
+
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """
+    unittest for class Base
+    """
+    # correct
+    # incorrect type of args
+    # incorrect number of args
