6d2df801e88ef061b899e5b1f699daf260fdba68
jfharris <jfharris1983@gmail.com>
2022-01-28 16:19:47 -0600

    Resolved previous errors and added str to get correct output

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,16 +5,17 @@ class Square
 
 
 from models.rectangle import Rectangle
-class Square:
+class Square(Rectangle):
     """
     class = Square
     inherits private attrs from Rectangle(height, width, x, y
     """
     def __init__(self, size, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        super().__init__(id)
-# resolve errors
-# add return statement
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        id2 = self.id
+        x = super().x
+        y = super().y
+        w2 = super().width
+        return "[Square] ({}) {}/{} - {}".format(id2, x, y, w2)
