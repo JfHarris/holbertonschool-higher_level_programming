becec8224a3299029e0a4ad14f99fd2abed9ed9c
jfharris <jfharris1983@gmail.com>
2022-01-28 01:21:16 -0600

    Added width, height, x, and y tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,10 +7,36 @@ import unittest
 from models.rectangle import Rectangle
 from models.base import Base
 
-class TestRectangle(unittest.TestCase):
+class TestRectangleclass(unittest.TestCase):
     """
     unittest for Rectangle class
     """
     # correct
     # all args incorrect
     # no args
+    def setUp(self):
+        self.inst = Rectangle(1, 2, 3, 4, 5)
+
+    def test_width(self):
+        """
+        test rectangle height
+        """
+        self.assertEqual(self.inst.width, 1)
+
+    def test_width(self):
+        """
+        test rectangle width
+        """
+        self.assertEqual(self.inst.height, 2)
+
+    def test_x(self):
+        """
+        text x
+        """
+        self.assertEqual(self.inst.x, 3)
+
+    def test_y(self):
+        """
+        test y
+        """
+        self.assertEqual(self.inst.y, 4)
