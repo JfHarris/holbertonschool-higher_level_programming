8391de1b52c5d9dda1c7b4f6e2970cf9219cea7c
jfharris <jfharris1983@gmail.com>
2022-01-27 22:18:25 -0600

    Changed not in to != in setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
-        if type(width) not in int:
+        if type(width) != int:
             raise TypeError("width must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
@@ -38,7 +38,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
-        if type(height) not in int:
+        if type(height) != int:
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
@@ -50,7 +50,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
-        if type(x) not in int:
+        if type(x) != int:
             raise TypeError("x must be an integer")
         if x < 0:
             raise ValueError("x must be >= 0")
@@ -62,8 +62,11 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
-        if type(y) not in int:
+        if type(y) != int:
             raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        return self.__width * self.__height
