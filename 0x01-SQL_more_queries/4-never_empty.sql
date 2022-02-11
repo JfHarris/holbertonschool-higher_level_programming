b28eaf606b8d52642bc783e718d874249d67dbe4
jfharris <jfharris1983@gmail.com>
2022-02-10 19:55:44 -0600

    changed 255 to 256

--- 0x01-SQL_more_queries/4-never_empty.sql
+++ 0x01-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- script that creates table
 --id_not_null on your MySQL server.
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
