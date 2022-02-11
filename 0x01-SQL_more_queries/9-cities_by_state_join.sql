12fbe5ddf3d01cfd60d9d7679607deeb391606c6
jfharris <jfharris1983@gmail.com>
2022-02-10 22:12:59 -0600

    Several edits

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- script that lists all cities
 -- contained in database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM cities RIGHT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
