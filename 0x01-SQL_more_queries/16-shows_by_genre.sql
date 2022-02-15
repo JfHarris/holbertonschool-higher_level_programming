0c66cdc048e729a626f5d0dcbdbf8e644629e902
jfharris <jfharris1983@gmail.com>
2022-02-14 22:35:00 -0600

    Changed inner join to left join

--- 0x01-SQL_more_queries/16-shows_by_genre.sql
+++ 0x01-SQL_more_queries/16-shows_by_genre.sql
@@ -2,8 +2,8 @@
 -- script that lists all shows, and all genres linked to that show from hbtn_0d_tvshows
 SELECT ts.`title`, ge.`name`
 FROM `tv_shows` AS ts
-INNER JOIN `tv_show_genres` AS sh
+LEFT JOIN `tv_show_genres` AS sh
 ON ts.`id` = sh.`show_id`
-INNER JOIN `tv_genres` AS ge
+LEFT JOIN `tv_genres` AS ge
 ON sh.`genre_id` = g.`id`
 ORDER BY ts.`title`, ge.`name`;
