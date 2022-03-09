16906c6b90d8dc4150b5d40fd84ec23a86d939c2
jfharris <jfharris1983@gmail.com>
2022-03-08 23:02:53 -0600

    edit typo in url

---
+++ 0x0C-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+
+import sys
+from requests.auth import HTTPBasicAuth
+import requests
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    token = sys.argv[2]
+    form = {'state': 'open'}
+    url = 'https://api.github.com/user'
+    new_req = requests.get(url, auth=HTTPBasicAuth(username, token) ,form=form)
+    ans = new_req.json()
+    print(ans.get('id'))
