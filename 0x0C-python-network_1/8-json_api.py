68003e7ca57e462f7d00e00d31b4ddb3475a0de5
jfharris <jfharris1983@gmail.com>
2022-03-08 23:05:44 -0600

    Pep8 2

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -14,8 +14,7 @@ if __name__ == '__main__':
         new_dict['q'] = sys.argv[1]
     else:
         new_dict['q'] = ""
-    new_req = requests.post('http://0.0.0.0:5000/search_user', info = new_dict)
-
+    new_req = requests.post('http://0.0.0.0:5000/search_user', info=new_dict)
     try:
         resp = new_req.json()
         if resp == {}:
