5edf3a80d0ac16b2634fc77456257d434a0af29d
jfharris <jfharris1983@gmail.com>
2022-03-10 15:38:08 -0600

    Edit overall

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     import urllib.request
     import sys
 
-    with urllib.request.urlopen(sys.argv[1]) as response:
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
         html = response.info()
-        print("{}".format(html.get('X-Request-ID')))
+    new = html.get("X-Request-Id")
+    print(new)
