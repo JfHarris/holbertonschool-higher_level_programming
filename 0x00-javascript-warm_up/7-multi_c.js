1a514f4be56f74724e88d8e5edc1e2db297bc5fa
jfharris <jfharris1983@gmail.com>
2022-04-04 16:55:38 -0500

    Format

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const strs = "C is fun";
+const strs = 'C is fun';
 const inte = process.argv[2];
 let x = 0;
 if (parseInt(inte)) {
-    while (x < inte) {
-        console.log(strs);
-        x++;
-    }
+  while (x < inte) {
+    console.log(strs);
+    x++;
+  }
 } else {
-    console.log("Missing number of occurrences");
+    console.log('Missing number of occurrences');
 }
