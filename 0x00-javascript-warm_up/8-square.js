27917213214c8006f7fef6aa4885dbbe2d09d3ed
jfharris <jfharris1983@gmail.com>
2022-04-04 17:15:33 -0500

    FORMAT

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 const sqChar = "X";
 const sqSize = process.argv[2];
 if (parseInt(sqSize)) {
-    for (let rowSq = 0; rowSq < sqSize; rowSq++) {
-        console.log(sqChar.repeat(sqSize));
-    }
+  for (let rowSq = 0; rowSq < sqSize; rowSq++) {
+    console.log(sqChar.repeat(sqSize));
+  }
 } else {
-    console.log("Missing size");
+    console.log('Missing size');
 }
