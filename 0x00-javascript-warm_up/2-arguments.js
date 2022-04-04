dba1cf5e88ace30af3f1bfdda0759669ce745bcf
jfharris <jfharris1983@gmail.com>
2022-04-04 16:42:39 -0500

    Edited argv range

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 let ans;
-if (process.argv.length < 1) {
-    ans = "No argument";
-} else if (process.argv.length === 1) {
-    ans = "Argument found";
+if (process.argv.length < 3) {
+    ans = 'No argument';
+} else if (process.argv.length === 3) {
+    ans = 'Argument found';
 } else {
-    ans = "Arguments found";
+    ans = 'Arguments found';
 }
 console.log(ans);
