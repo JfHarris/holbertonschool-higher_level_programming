c86a191b514b6cb486c886d9825220fb644685f9
jfharris <jfharris1983@gmail.com>
2022-04-04 17:35:33 -0500

    Added isNaN

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+function factorial (inte) {
+  if (isNaN(inte)) {
+    return 1;
+  }
+  if (inte < 0) {
+    return -1;
+  }
+  if (inte === 0) {
+    return 1;
+  } else {
+    return inte * factorial(inte - 1);
+  }
+}
+console.log(factorial(process.argv[2]));
