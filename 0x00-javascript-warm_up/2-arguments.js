475d8c3da06705c5048805d06ff77606c732351f
jfharris <jfharris1983@gmail.com>
2022-04-04 01:17:33 -0500

    Edit 1

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let ans;
+if (process.argv.length < 1) {
+    ans = "No argument";
+} else if (process.argv.length === 1) {
+    ans = "Argument found";
+} else {
+    ans = "Arguments found";
+}
+console.log(ans);
