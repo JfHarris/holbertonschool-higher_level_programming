c989ace2f3150276c08b932b5acb0e3eda666dcf
jfharris <jfharris1983@gmail.com>
2022-04-04 02:26:05 -0500

    Added var for rows

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const sqChar = "X";
+const sqSize = process.argv[2];
+if (parseInt(sqSize)) {
+    for (let rowSq = 0; rowSq < sqSize; rowSq++) {
+        console.log(sqChar.repeat(sqSize));
+    }
+} else {
+    console.log("Missing size");
+}
