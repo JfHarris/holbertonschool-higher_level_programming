dd52e43710b4484c02dc4084a94c697d8f8f6b26
jfharris <jfharris1983@gmail.com>
2022-04-05 17:00:25 -0500

    Edited repeat statements

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// class Square that a square & inherits from Square of 5-square.js
+const SquareBase = require('./5-square');
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    process.stdout.write((c.repeat(this.width) + '\n').repeat(this.height));
+  }
+}
+module.exports = Square;
