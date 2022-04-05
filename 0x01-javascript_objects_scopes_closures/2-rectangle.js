1546b9abdc0631739ffe181626f506aa4337343e
jfharris <jfharris1983@gmail.com>
2022-04-05 01:29:29 -0500

    Capitalized X

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// adding to Rectangle--If w or h == 0 or a - int, create empty obj
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
