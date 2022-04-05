b88663a3655dd26dd35898869d2bf150a394e218
jfharris <jfharris1983@gmail.com>
2022-04-05 17:44:13 -0500

    Changed for loop

--- 0x01-javascript_objects_scopes_closures/8-esrever.js
+++ 0x01-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+//  function that returns the reversed version of a list
+exports.esrever = function (list) {
+  const revList = list.slice();
+  let x = 0;
+  for (x = 0; x < list.length; x++) {
+    list[x] = revList[list.length - x - 1];
+  }
+  return list;
+};
