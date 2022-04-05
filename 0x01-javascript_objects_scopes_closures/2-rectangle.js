389e1ba5cbb45bac132faada0f90ab966c54e796
jfharris <jfharris1983@gmail.com>
2022-04-05 01:40:12 -0500

    Capitalized X and formatting

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// adding to Rectangle--If w or h == 0 or a - int, create empty obj
+// adding to Rectangle---If w or h == 0 or a - int, create empty obj
 class Rectangle {
   constructor (w, h) {
     if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// adding to Rectangle--printing rectangle using X
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(
+      ('X'.repeat(this.width) + '\n')
+        .repeat(this.height)
+        .split('')
+        .slice(0, -1)
+        .join('')
+    );
+  }
+}
+module.exports = Rectangle;
