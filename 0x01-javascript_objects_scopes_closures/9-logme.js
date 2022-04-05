2151bbeb003e4d8bdf22c5af14f849c6495bdb91
jfharris <jfharris1983@gmail.com>
2022-04-05 18:06:46 -0500

    fixed console statement

--- 0x01-javascript_objects_scopes_closures/9-logme.js
+++ 0x01-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+// function prints the # of args already printed and new arg val
+exports.logMe = function (item) {
+  this.insts = (this.insts || 0) + 1;
+  console.log(`${this.insts - 1}: ${item}`);
+};
