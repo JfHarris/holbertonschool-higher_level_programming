e0ea2d34ca3b1dc5fb8c612bbf786c1684cb0391
jfharris <jfharris1983@gmail.com>
2022-04-05 19:00:18 -0500

    Formatting

--- 0x01-javascript_objects_scopes_closures/10-converter.js
+++ 0x01-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+// function that converts # from base 10 to another base passed as arg
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
