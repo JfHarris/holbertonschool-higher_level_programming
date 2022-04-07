a0c2759c2614bd36e60b241604b039bbcf6b007a
jfharris <jfharris1983@gmail.com>
2022-04-07 01:47:13 -0500

    Several edits

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const f_path = process.argv[2];
+fs.readFile(f_path, 'utf8', function read (error, fileInfo) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const content = fileInfo;
+  console.log(content);
+});
