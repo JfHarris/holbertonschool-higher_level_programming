c87d4aab222564edea5e9a7f6f07fbc7ed041752
jfharris <jfharris1983@gmail.com>
2022-04-07 17:29:50 -0500

    set counter to iterate

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+//prints the # of movies where character “Wedge Antilles” is present
+const request = require('request');
+const urlGet = process.argv[2];
+request(urlGet, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const results = JSON.parse(body).results;
+  let counter = 0;
+  for (const lists of results) {
+    for (const characters of lists.characters) {
+      if (characters.includes('18')) {
+        counter = counter + 1;
+      }
+    }
+  }
+  console.log(counter);
+});
