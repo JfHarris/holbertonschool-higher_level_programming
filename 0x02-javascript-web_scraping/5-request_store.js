484951493a85c961269a4da8b16f34046ada7d72
jfharris <jfharris1983@gmail.com>
2022-04-07 17:46:10 -0500

    Typo fix

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+// gets the contents of a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+const urlGet = process.argv[2];
+const fPath = process.argv[3];
+request.get(urlGet, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(fPath, body, 'utf8', function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
