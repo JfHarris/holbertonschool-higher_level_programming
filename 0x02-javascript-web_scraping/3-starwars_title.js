7be84168c58fc1102e42d52d63544294e5daedff
jfharris <jfharris1983@gmail.com>
2022-04-07 16:56:27 -0500

    Fixed URL that I can't even copy and paste

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// prints title of a Star Wars movie where the ep # matches given int
+const request = require('request');
+const urlEnd = 'https://swapi-api.hbtn.io/api/films/';
+const epNum = process.argv[2];
+request(urlEnd + epNum, function (error, response, body) {
+  error && console.log(error);
+  console.log(JSON.parse(body).title);
+});
