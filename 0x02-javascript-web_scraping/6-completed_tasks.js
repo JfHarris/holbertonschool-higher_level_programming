44b652c4e079ab0d94ac0c9d7e57f2e8bcf40df8
jfharris <jfharris1983@gmail.com>
2022-04-07 17:48:39 -0500

    Added else loop

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+//computes the number of tasks completed by user id
+const request = require('request');
+const urlGet = process.argv[2];
+request.get(urlGet, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const users = JSON.parse(body).reduce((info, iter) => {
+    if (iter.completed) {
+      if (info[iter.userId]) {
+        info[iter.userId]++;
+      } else {
+        info[iter.userId] = 1;
+      }
+    }
+    return info;
+  }, {});
+  console.log(users);
+});
