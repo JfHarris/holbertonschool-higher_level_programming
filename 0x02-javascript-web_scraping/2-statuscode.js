5a44ebcfc6cf2dbed7cdb68b49804aff60d3e065
jfharris <jfharris1983@gmail.com>
2022-04-07 16:23:04 -0500

    Added space after code:

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const urlGet = process.argv[2];
+const request = require('request');
+request(urlGet, function (error, response) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('code: ' + response.statusCode);
+});
