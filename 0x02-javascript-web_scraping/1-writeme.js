c1f141a2f00452f0b1850ad581f9d70d6419cf1b
jfharris <jfharris1983@gmail.com>
2022-04-07 15:55:32 -0500

    fixed argv range for write

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const fs = require('fs');
+const fPath = process.argv[2];
+const fWrite = process.argv[3];
+fs.writeFile(fPath, fWrite, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
