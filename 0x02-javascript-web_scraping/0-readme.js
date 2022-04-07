d19e2f8bb23c1f5590ff2289b4aa49f76220f1e5
jfharris <jfharris1983@gmail.com>
2022-04-07 15:15:58 -0500

    Semistandard

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-const f_path = process.argv[2];
-fs.readFile(f_path, 'utf8', function read (error, fileInfo) {
+const fPath = process.argv[2];
+fs.readFile(fPath, 'utf8', function read (error, fileInfo) {
   if (error) {
     console.log(error);
     return;
