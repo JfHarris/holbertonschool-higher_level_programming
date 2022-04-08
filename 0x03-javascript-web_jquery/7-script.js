19a0cbb25d5f0f946225f1ba0ae72dc575667663
jfharris <jfharris1983@gmail.com>
2022-04-07 23:20:36 -0500

    fixed DIV#character to get output

---
+++ 0x03-javascript-web_jquery/7-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  const charName = data.name
+  $('DIV#character').text(charName);
+});
+// fetches the char name from URL: https://swapi-api.hbtn.io/api/people/5/?format=json
